5ac1f18e98a560c3bf7b10342e3ddca1401e8474
Andre Minoru <andre.inafuku@gmail.com>
2022-03-28 19:50:51 -0300

    remove files

--- CTAS-insert-01.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO TESTE(id, nome) values (1, 'TESTE');
-COMMIT;
--- CTAS-update-01.sql
+++
@@ -1,7 +0,0 @@
--- Primeira alteração
--- Segunda alteração
--- Terceira alteração
-update cyberbank_core.account
-set nome='Andre'
-where id = 1234124;
-commit;
